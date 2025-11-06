61b68ad8863cf9817aaa406e170b4a40b2e89c42
joaovitormv <joaovitorjjabuti@gmail.com>
2025-11-06 00:21:17 +0000

    feat: implementa handleSearch para encontrar paises apos chamar a API

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,62 @@
 import React, { useState } from 'react';
 import { FlatList, SafeAreaView, StyleSheet, useWindowDimensions } from "react-native";
-import { mockCountries } from '../src/api/mockData';
+import { searchCountryByName } from '@/src/api/restCountries';
 import Card from '../src/components/card';
 import Header from "../src/components/header";
 import SearchBar from "../src/components/searchBar";
 import { Country } from './types';
- 
+
 const CARD_MIN_WIDTH = 180;
 const LIST_PADDING = 8;
 
 
 export default function Index() {
-    const [countries, setCountries] = useState<Country[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string | null>(null);
-    
-    const {width} = useWindowDimensions();
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-    const numColumns = Math.max(1, Math.floor(width / CARD_MIN_WIDTH));
-    const columnWidth = (width - (LIST_PADDING * 2)) / numColumns
+  const { width } = useWindowDimensions();
 
-    const handleSearch = (searchText: string ) =>{
-      console.log(searchText); //terminar isso
+  const numColumns = Math.max(1, Math.floor(width / CARD_MIN_WIDTH));
+  const columnWidth = (width - (LIST_PADDING * 2)) / numColumns
+
+  async function handleSearch(searchText: string){
+    setIsLoading(true);
+    setError(null);
+    setCountries([]);
+
+    try{
+      const data = await searchCountryByName(searchText);
+      setCountries(data);
+    }catch(e){
+      if(e instanceof Error){
+        setError(e.message);
+      }else{
+        setError("Erro desconhecido");
+      }
+    } finally{
+      setIsLoading(false);
     }
+  }
 
-    return (
+
+  return (
     <SafeAreaView>
-        <Header/>
-        <SearchBar onSearch={handleSearch}/>
-        <FlatList
-            data={countries}
-            renderItem={({item}) => <Card item={item} width={columnWidth}/>}
-            keyExtractor={(item) => item.name.common}
-            style={styles.list}
-            numColumns={numColumns}
-            key={numColumns}
-            
-        />
+      <Header />
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        data={countries}
+        renderItem={({ item }) => <Card item={item} width={columnWidth} />}
+        keyExtractor={(item) => item.name.common}
+        style={styles.list}
+        numColumns={numColumns}
+        key={numColumns}
+
+      />
     </SafeAreaView>
-    
-);
-}const styles = StyleSheet.create({
+
+  );
+} const styles = StyleSheet.create({
   list: {
     paddingHorizontal: 8,
   },
--- src/api/restCountries.ts
+++ src/api/restCountries.ts
@@ -3,4 +3,13 @@ const API_URL = 'https://restcountries.com/v3.1';
 
 export const searchCountryByName = async (name: string): Promise<Country[]> =>{
     const response = await fetch(`${API_URL}/name/${name.trim()}`);
+
+    if(!response.ok){
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'País não encontrado');
+    }
+
+    const data: Country[] = await response.json();
+    return data;
+
 }
